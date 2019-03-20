6a22047a5d893671aeb02d641936258fcbd9bd94
svber <berningsven@gmail.com>
2019-03-20 13:47:20 +0100

    [added] interupt

--- ThereminCopy/ThereminCopy/main.c
+++ ThereminCopy/ThereminCopy/main.c
@@ -1,104 +1,59 @@
 /*
- * ThereminCopy.c
- *
- * Created: 20/03/2019 10:05:39
- * Author : Tim
- */ 
+* ThereminCopy.c
+*
+* Created: 20/03/2019 10:05:39
+* Author : Tim
+*/
 
 #define F_CPU 8000000UL
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
+int duration;
+int distance;
+
 void echoNoTimer(void){
 	/* Replace with your application code */
+
 	DDRD = 0b00000100;
 	DDRA = 0b11111111;
 	DDRB = 0b11111111;
+	
+	EICRB = 0b00000001;
+	EIMSK = 0b00010000;
+	sei();
+	
 	while (1)
 	{
-		int duration, distance;
+		
 		
 		//digitalWrite(trigPin, HIGH);
-		PORTD = 0b00000100;
+		PORTE = 0b00000100;
 		
 		//delayMicroseconds(1000);
 		_delay_us(1000);
 		
 		//digitalWrite(trigPin, LOW);
-		PORTD = 0b00000000;
+		PORTE = 0b00000000;
 		
-		//duration = pulseIn(echoPin, HIGH);
-		
-		
-		duration = 0;
-		int temp;
-		temp = PIND & 0b00000001;
-		while(!temp){
-			temp = PIND & 0b00000001;
-		}
-		while(temp && duration < 10000){
-			temp = PIND & 0b00000001;
-			_delay_us(1);
-			duration++;
-		}
-		
-		distance = (duration/2) / 29.1;
-		PORTB = duration/10;
-		if (distance >= 200 || distance <= 0){
-			//Serial.println("Out of range");
-			PORTA = 0b11111111;
-		}
-		else {
-			//Serial.print(distance);
-			//Serial.println(" cm");
-			PORTA = distance;
-		}
-		_delay_ms(200);
+		_delay_ms(500);
 	}
 }
 
-void echoTimer(void){
-	DDRD = 0b00000100;
-	DDRA = 0b11111111;
-	DDRB = 0b11111111;
-	
-	while (1)
+
+ISR(INT4_vect)
+{
+	if (PINE & 0b00010000)
 	{
-		int duration, distance;
-		
-		//digitalWrite(trigPin, HIGH);
-		PORTD = 0b00000100;
-		
-		//delayMicroseconds(1000);
-		_delay_us(1000);
-		
-		//digitalWrite(trigPin, LOW);
-		PORTD = 0b00000000;
-		
-		//duration = pulseIn(echoPin, HIGH);
-		
-		
 		duration = 0;
-		int temp;
-		temp = PIND & 0b00000001;
-		
-	
-		
-		while(!temp){
-			temp = PIND & 0b00000001;
-		}
-		
 		TCNT1 = 0;
-		TCCR1B |= 0b10;
-		while(temp && duration < 10000){
-			temp = PIND & 0b00000001;
-			//_delay_us(1);
-			//duration++;
-		}
+		TCCR1B |= 0b10;	
+	}else
+	{
 		TCCR1B = 0;
 		duration = TCNT1;
-		
+		//todo get timer number
 		distance = (duration/2) / 29.1;
 		PORTB = duration/10;
 		if (distance >= 200 || distance <= 0){
@@ -110,13 +65,12 @@ void echoTimer(void){
 			//Serial.println(" cm");
 			PORTA = distance;
 		}
-		_delay_ms(200);
 	}
 }
 
 int main(void)
 {
-    echoTimer();
+    echoNoTimer();
 }
 
 
