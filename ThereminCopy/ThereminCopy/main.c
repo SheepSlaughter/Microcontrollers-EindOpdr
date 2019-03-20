d2517e08e941b9e8a906fd5f64482c0f45f982e2
Tim <vries.timothy.de@gmail.com>
2019-03-20 10:07:43 +0100

    [init] Code

---
+++ ThereminCopy/ThereminCopy/main.c
@@ -0,0 +1,18 @@
+/*
+ * ThereminCopy.c
+ *
+ * Created: 20/03/2019 10:05:39
+ * Author : Tim
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
