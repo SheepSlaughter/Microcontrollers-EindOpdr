5377e1ac9646311c9d665aa72a2715849f352d11
Tim <vries.timothy.de@gmail.com>
2019-03-29 10:35:59 +0100

    aaaaaaaahh

--- ThereminCopy/ThereminCopy/main.c
+++ ThereminCopy/ThereminCopy/main.c
@@ -50,7 +50,61 @@ void echoNoTimer(void){
 		_delay_ms(500);
 }
 
-
+void echoTimer(void){
+	DDRD = 0b00000100;
+	DDRA = 0b11111111;
+	DDRB = 0b11111111;
+	
+	while (1)
+	{
+		int duration, distance;
+		
+		//digitalWrite(trigPin, HIGH);
+		PORTD = 0b00000100;
+		
+		//delayMicroseconds(1000);
+		_delay_us(1000);
+		
+		//digitalWrite(trigPin, LOW);
+		PORTD = 0b00000000;
+		
+		//duration = pulseIn(echoPin, HIGH);
+		
+		
+		duration = 0;
+		int temp;
+		temp = PIND & 0b00000001;
+		
+		
+		
+		while(!temp){
+			temp = PIND & 0b00000001;
+		}
+		
+		TCNT1 = 0;
+		TCCR1B |= 0b10;
+		while(temp && duration < 10000){
+			temp = PIND & 0b00000001;
+			//_delay_us(1);
+			//duration++;
+		}
+		TCCR1B = 0;
+		duration = TCNT1;
+		
+		distance = (duration/2) / 29.1;
+		PORTB = duration/10;
+		if (distance >= 200 || distance <= 0){
+			//Serial.println("Out of range");
+			PORTA = 0b11111111;
+		}
+		else {
+			//Serial.print(distance);
+			//Serial.println(" cm");
+			PORTA = distance;
+		}
+		_delay_ms(200);
+	}
+}
 ISR(INT4_vect)
 {
 	if (PINE & 0b00010000)
